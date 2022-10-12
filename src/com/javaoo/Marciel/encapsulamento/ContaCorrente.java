7e45978d6484390ee95eb484fae1ac0b8955c0a3
Marciel Bezerra <marcielgestao2007.1@gmail.com>
2022-10-12 17:17:04 -0300

    Adicionando Marciel

---
+++ src/com/javaoo/Marciel/encapsulamento/ContaCorrente.java
@@ -0,0 +1,39 @@
+package marcielbezerra.encapsulamento;
+
+public class ContaCorrente {
+	private int numeroConta;
+	private double saldoConta = 0;
+	
+	
+	public ContaCorrente(int numeroConta, double saldoConta) {
+		this.numeroConta = numeroConta;
+		this.saldoConta = saldoConta;
+	}
+
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+
+	public double getSaldoConta() {
+		if (saldoConta <= 0) {
+			return 100;
+		}
+		return saldoConta;
+	}
+
+
+	public void setSaldoConta(double saldoConta) {
+		this.saldoConta = saldoConta;
+	}
+	
+	
+	
+	
+}
---
+++ src/com/javaoo/Marciel/encapsulamento/TestarConta.java
@@ -0,0 +1,17 @@
+package marcielbezerra.encapsulamento;
+
+public class TestarConta extends ContaCorrente {
+
+	public TestarConta(int numeroConta, double saldoConta) {
+		super(numeroConta, saldoConta);
+		
+	}
+
+	public static void main(String[] args) {
+		TestarConta primeirConta = new TestarConta(8955544, 0);
+		
+		System.out.println("Olá o saldo atual de sua conta " + primeirConta.getNumeroConta() + " é: ");
+		System.out.println(primeirConta.getSaldoConta());
+	}
+
+}
